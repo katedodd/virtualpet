85fb9e59af7d4929cc55748e2b82e5de033ac025
Kate Dodd <kate.dodd@7factor.io>
2021-05-18 20:42:17 -0400

    First commit

---
+++ src/components/drawing/drawing.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="canvas">
+
+  </div>
+</template>
+
+<script>
+import "./drawing.css";
+
+export default {
+  name: "drawing",
+  methods: {
+
+  },
+  data() {
+    return {
+      message: "Hello, child!",
+      vueCanvas: null,
+    }
+  },
+  mounted() {
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+    this.vueCanvas = ctx;
+
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    ctx.fillRect(100, 100, 300, 300);
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(100, 100, 300, 300);
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/DrawingPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>trying to be rad</h1>
+    <drawing></drawing>
+  </div>
+</template>
+
+<script>
+import drawing from "../components/drawing/drawing";
+export default {
+  name: "DrawingPage",
+  components: {
+    drawing
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
